6f86b0f291774624e1699495124940a7b28fcd39
Halil Oymaci <hioymaci@gmail.com>
2024-10-09 23:59:23 +0300

    [pgqiA80J] initial commit

---
+++ src/main/kotlin/com/oymaci/stylegpt/StylegptApplication.kt
@@ -0,0 +1,13 @@
+package com.oymaci.stylegpt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
+import java.io.IO.println
+
+@SpringBootApplication
+class StylegptApplication
+
+fun main(args: Array<String>) {
+	println("Hello, World!")
+//	runApplication<StylegptApplication>(*args)
+}
---
+++ src/test/kotlin/com/oymaci/stylegpt/StylegptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oymaci.stylegpt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StylegptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
